20a623458dfa6e2e2cb64f2e07df88ccbf1e1dc5
Ethan Hackney <ethanhackney@pop-os.localdomain>
2023-11-17 22:35:50 -0800

    added code to join on a single field

--- main.cc
+++ main.cc
@@ -4,6 +4,8 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <err.h>
+#include <sysexits.h>
 
 // small program, so this is not a big deal
 using namespace std;
@@ -17,29 +19,40 @@ static vector<unordered_map<string,string>> g_rows;
 // break line into fields
 static vector<string> fields(char *line);
 
+// versions of stdio functions that print error message
+// and exit on failures
+static FILE *e_fopen(const char *path, const char *mode);
+static void e_fclose(FILE *fp);
+static char *e_fgets(char *buf, size_t size, FILE *fp);
+
+// get index of attribute
+static size_t attr_index(const vector<string> &attrs, const string &attr);
+
 int main(int argc, char **argv)
 {
-        if (argc != 3) {
-                fprintf(stderr, "Usage: ./a.out file0 file1\n");
+        if (argc != 4) {
+                fprintf(stderr, "Usage: ./a.out file0 file1 attribute\n");
                 exit(1);
         }
 
-        auto fp = fopen(argv[1], "r");
-        if (fp == NULL) {
-                fprintf(stderr, "could not open %s\n", argv[1]);
-                exit(1);
-        }
+        auto fp = e_fopen(argv[1], "r");
 
         // first line has attribute names
         char buf[BUFSIZ];
-        if (fgets(buf, sizeof(buf), fp) == NULL) {
+        if (e_fgets(buf, sizeof(buf), fp) == NULL) {
                 fprintf(stderr, "%s is empty\n", argv[1]);
                 exit(1);
         }
         auto attrnames = fields(buf);
 
+        // make sure first file has join attribute
+        if (attr_index(attrnames, argv[3]) == attrnames.size()) {
+                fprintf(stderr, "%s does not have attribute %s\n", argv[1], argv[3]);
+                exit(1);
+        }
+
         // read first database into index
-        while (fgets(buf, sizeof(buf), fp) != NULL) {
+        while (e_fgets(buf, sizeof(buf), fp) != NULL) {
                 auto attrvalues = fields(buf);
 
                 // allocate a new row
@@ -54,79 +67,82 @@ int main(int argc, char **argv)
                         g_rows[rowidx][name] = value;
                 }
         }
-        if (ferror(fp)) {
-                fprintf(stderr, "could not read %s\n", argv[1]);
-                exit(1);
-        }
-        if (fclose(fp) < 0) {
-                fprintf(stderr, "could not close %s\n", argv[1]);
-                exit(1);
-        }
+        e_fclose(fp);
 
-        fp = fopen(argv[2], "r");
-        if (fp == NULL) {
-                fprintf(stderr, "could not open %s\n", argv[2]);
-                exit(1);
-        }
+        fp = e_fopen(argv[2], "r");
 
-        if (fgets(buf, sizeof(buf), fp) == NULL) {
+        // get attributes of next database
+        if (e_fgets(buf, sizeof(buf), fp) == NULL) {
                 fprintf(stderr, "%s is empty\n", argv[2]);
                 exit(1);
         }
-        auto attrnames2  = fields(buf);
+        auto attrnames2 = fields(buf);
 
-        // find matching attributes
-        auto matches = std::vector<size_t>(attrnames.size());
-        auto nr_matches = 0;
-        size_t i = 0;
-        for (; i < attrnames.size(); ++i) {
-                for (size_t j = 0; j < attrnames2.size(); ++j) {
-                        if (attrnames2[j] == attrnames[i]) {
-                                matches[i] = 1;
-                                ++nr_matches;
-                                break;
-                        }
-                }
-        }
-        // if there are any more attributes, then the rest are mismatches
-        for (; i < attrnames.size(); ++i)
-                matches[i] = 0;
-        if (nr_matches == 0) {
-                fprintf(stderr, "databases have no matching attributes\n");
+        // make sure second database has attribute, and get
+        // index of field we are joining on
+        auto attridx = attr_index(attrnames2, argv[3]);
+        if (attridx == attrnames2.size()) {
+                fprintf(stderr, "%s does not have attribute %s\n", argv[2], argv[3]);
                 exit(1);
         }
 
-        while (fgets(buf, sizeof(buf), fp) != NULL) {
+        // actual hash join
+        auto join_attr = attrnames2[attridx];
+        while (e_fgets(buf, sizeof(buf), fp) != NULL) {
                 auto attrvalues = fields(buf);
 
-                for (size_t attr = 0; attr < attrnames.size(); ++attr) {
-                        if (matches[attr] == 0)
-                                continue;
+                // see if there are any rows that have this
+                // attribute with this value
+                auto value = attrvalues[attridx];
+                auto p = g_index[join_attr].find(value);
+                if (p == g_index[join_attr].end())
+                        continue;
 
-                        auto name = attrnames[attr];
-                        auto value = attrvalues[attr];
-                        auto p = g_index.find(name);
-                        if (p->second.size() == 0)
-                                continue;
-
-                        auto rows = g_index[name][value];
-                        for (size_t r = 0; r < rows.size(); ++r) {
-                                for (auto f : g_rows[rows[r]])
-                                        printf("%s ", f.second.c_str());
-                                for (size_t i = 0; i < attrvalues.size(); ++i)
+                // iterate through all rows that match
+                auto rows = g_index[join_attr][value];
+                for (size_t r = 0; r < rows.size(); ++r) {
+                        for (auto attr : g_rows[rows[r]])
+                                printf("%s ", attr.second.c_str());
+                        for (size_t i = 0; i < attrvalues.size(); ++i) {
+                                // do not print join field twice because
+                                // we already printed it when we did
+                                // the row
+                                if (i != attridx)
                                         printf("%s ", attrvalues[i].c_str());
-                                printf("\n");
                         }
+                        printf("\n");
                 }
         }
-        if (ferror(fp)) {
-                fprintf(stderr, "could not read %s\n", argv[2]);
-                exit(1);
-        }
-        if (fclose(fp) < 0) {
-                fprintf(stderr, "could not close %s\n", argv[2]);
-                exit(1);
-        }
+        e_fclose(fp);
+}
+
+static FILE *
+e_fopen(const char *path, const char *mode)
+{
+        auto fp = fopen(path, mode);
+
+        if (fp == NULL)
+                err(EX_SOFTWARE, "fopen(%s, %s)", path, mode);
+
+        return fp;
+}
+
+static void
+e_fclose(FILE *fp)
+{
+        if (fclose(fp) < 0)
+                err(EX_SOFTWARE, "fclose()");
+}
+
+static char *
+e_fgets(char *buf, size_t size, FILE *fp)
+{
+        auto p = fgets(buf, size, fp);
+
+        if (ferror(fp))
+                err(EX_SOFTWARE, "fgets()");
+
+        return p;
 }
 
 static vector<string> fields(char *line)
@@ -151,4 +167,16 @@ static vector<string> fields(char *line)
                 fields.push_back({base});
 
         return fields;
+}
+
+static size_t attr_index(const vector<string> &attrs, const string &attr)
+{
+        size_t i;
+
+        for (i = 0; i < attrs.size(); ++i) {
+                if (attrs[i] == attr)
+                        break;
+        }
+
+        return i;
 }
